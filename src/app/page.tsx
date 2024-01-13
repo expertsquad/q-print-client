3b626940b0414e08881b5f2420843e3bf0276999
Zayed Hossain <zayedhossain120@gmail.com>
2024-01-13 18:36:00 +0600

    added from zayed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="text-center text-5xl font-bold pt-11">
-      Q-print is comming .....
+      Q-print is comming .... By ExperSquad
     </main>
   );
 }
