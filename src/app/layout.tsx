31a5e60349b0ca12049b6655f81f4503db7ddbb4
Jaker Hossain <mrporanyt120@gmail.com>
2024-01-13 19:31:09 +0600

    added google font  poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Q Print",
   description: "Q Print | get all kind of pring online order it from here",
 };
 
-const Mainlayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 };
 
-export default Mainlayout;
+export default RootLayout;
