4b2d03bff9735229d533cf4710d8335ab6a43029
Zayed Hossain <zayedhossain120@gmail.com>
2024-01-13 18:51:48 +0600

    loading added

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="items-center ">
+      <span className="loading loading-spinner text-primary"></span>
+    </div>
+  );
+};
+
+export default loading;
