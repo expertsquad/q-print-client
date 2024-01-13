e0e756d4205039b2cb6e516afaa1ba093377901a
Zayed Hossain <zayedhossain120@gmail.com>
2024-01-13 19:41:59 +0600

    font clamp added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <main className="text-5xl font-bold items-center">
+    <main className="text-5xl font-bold items-center [font-size:_clamp(2em,5vw,10em)]">
       Q print is comming ...
     </main>
   );
