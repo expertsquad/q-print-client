8a7c10bcba8c8ceb930cff09ad5bb707b406a4ef
Jaker Hossain <mrporanyt120@gmail.com>
2024-01-16 21:37:34 +0600

    working on shop by category

---
+++ src/components/ShopCategory/ShopByCategory.tsx
@@ -0,0 +1,26 @@
+import { shopByCategories } from "@/constants";
+import ShopCategoryCard from "./ShopCategoryCard";
+
+const ShopByCategory = () => {
+  return (
+    <section className="mb-16">
+      <div>
+        <div className="flex items-center justify-center flex-col mb-6">
+          <p className="[font-size:_clamp(0.75em,5vw,1em)] text-gray-500">
+            Shop by Category
+          </p>
+          <h3 className="[font-size:_clamp(1em,5vw,1.6em)] font-bold">
+            Browser Our Hottest Categories
+          </h3>
+        </div>
+        <div className="flex gap-4 justify-between overflow-scroll no-scrollbar ">
+          {shopByCategories.map((item) => (
+            <ShopCategoryCard key={item._id} category={item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ShopByCategory;
