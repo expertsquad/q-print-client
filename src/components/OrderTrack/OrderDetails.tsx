d4f2ebbe3947e31574cbf589d3621f761f0d25b8
Jaker Hossain <mrporanyt120@gmail.com>
2024-01-21 13:13:19 +0600

    working on order track page

--- src/components/OrderTrack/OrderDetails.tsx
+++ src/components/OrderTrack/OrderDetails.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const OrderDetails = () => {
-  return <div>Order Details</div>;
+  return (
+    <div className="border rounded-xl mb-7">
+      <h4 className="border-b py-4 md:py-5 px-7">Order Details</h4>
+      {/* ==order-details order-summary order-items== */}
+      <div className="order-details order-summary order-items flex-col md:flex-col-reverse">
+        <div className="flex flex-col bg-slate-500">
+          <div className="user-details py-4 md:py-5 px-7">Order Details</div>
+          <div>Order Items</div>
+        </div>
+        {/* ==order summary== */}
+        <div className="bg-green-400">Order Summary</div>
+      </div>
+    </div>
+  );
 };
 
 export default OrderDetails;
