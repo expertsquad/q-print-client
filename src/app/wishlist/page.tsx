4d713bea743f2a10db7a60f887f5fba8e214f150
Jaker Hossain <mrporanyt120@gmail.com>
2024-01-26 18:51:53 +0600

    working on view wishlist

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Wishlist = () => {
   return (
-    <div>
-      <h2></h2>
+    <div className="max-w-[1280px] mx-auto">
+      <h2 className="text-black text-opacity-80 text-xl md:text-3xl mb-10">
+        My Wishlist
+      </h2>
+      <div className="bg-[#F8F8F8] hidden md:block ">
+        <h6>Product Name</h6>
+        <h6>Price</h6>
+        <h6>Stock Status</h6>
+        <h6>Action</h6>
+      </div>
     </div>
   );
 };
