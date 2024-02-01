ec9f3178fa3c8121e67bf8054cd6096ed1af7a82
Zayed Hossain <zayedhossain120@gmail.com>
2024-02-01 09:35:59 +0600

    fixed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "daisyui.com",
-      "freepngimg.com",
-      "http://www.w3.org/2000/svg",
-      "www.transparentpng.com",
-      "images.unsplash.com",
-      "img.freepik.com",
-      "w7.pngwing.com",
-      "images.pexels.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
