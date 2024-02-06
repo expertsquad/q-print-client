fa56d90dcdd19a82dca5f3e5b537ba21a5c7a00f
Zayed Hossain <zayedhossain120@gmail.com>
2024-02-06 10:53:09 +0600

    coursel fixed

--- src/components/carousel/BrandCarousel.tsx
+++ src/components/carousel/BrandCarousel.tsx
@@ -8,228 +8,142 @@ const BrandCarousel: React.FC<BrandCarouselProps> = () => {
   return (
     <div className="logos overflow-hidden mx-auto max-w-[1280px] lg:mt-20 md:mt-20 mt-8">
       <div className="logos-slide shrink-0 md:w-screen overflow-hidden">
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
       </div>
       <div className="logos-slide shrink-0 md:w-screen overflow-hidden">
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
-        <div className="h-5 w-7 md:h-12 md:w-16">
-          <Image
-            alt="Brand Carousel"
-            // width={170}
-            // height={120}
-            fill
-            objectFit="contain"
-            src="https://pngimg.com/uploads/nike/nike_PNG16.png"
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </div>
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          sizes="(max-width: 170px) 100vw, 33vw"
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
+        <Image
+          alt="Brand Carousel"
+          width={170}
+          height={120}
+          src="https://pngimg.com/uploads/nike/nike_PNG16.png"
+        />
       </div>
     </div>
   );
