b901c6ea963438ccfe30d35e4cf577c988da9917
Shariful525 <sharifulafsar0@gmail.com>
2024-04-30 17:10:04 +0600

    policy updated

--- src/app/(mainlayout)/privacy-policy/page.tsx
+++ src/app/(mainlayout)/privacy-policy/page.tsx
@@ -12,70 +12,74 @@ const PrivacyPolicy = () => {
                 <span>Last updated: 25 April 2024</span>
             </div>
 
+            {/* instruction */}
             <div className="mt-16 ">
                 <div className=' flex flex-col gap-2.5'>
                     <h2 className="font-bold md:text-xl text-lg">Introduction</h2>
                     <p>Welcome to Q-Print, your trusted source for printers and printer cartridges. We are committed to protecting the privacy of our customers and ensuring the security of their personal information. This Privacy Policy details how we collect, use, and safeguard the information you provide when you use our website and services.</p>
                 </div>
             </div>
+            {/* instruction */}
 
-            <div>
-                <h2 className='text-3xl font-bold'>Information We Collect</h2>
+            <div className="flex flex-col gap-16">
+                <div>
+                    <h2 className='text-3xl font-bold'>Information We Collect</h2>
 
-                {
-                    infoWeCollect?.map((info: any, index: number) => {
-                        return (
-                            <PrivacyContent key={index} data={info} />
-                        )
+                    {
+                        infoWeCollect?.map((info: any, index: number) => {
+                            return (
+                                <PrivacyContent key={index} data={info} />
+                            )
 
-                    })
-                }
-            </div>
+                        })
+                    }
+                </div>
 
-            <div>
-                <h2 className='text-3xl font-bold'>How We Use Your Information</h2>
+                <div>
+                    <h2 className='text-3xl font-bold'>How We Use Your Information</h2>
 
-                {
-                    weUseInfo?.map((info: any, index: number) => {
-                        return (
-                            <PrivacyContent key={index} data={info} />
-                        )
-                    })
-                }
-            </div>
+                    {
+                        weUseInfo?.map((info: any, index: number) => {
+                            return (
+                                <PrivacyContent key={index} data={info} />
+                            )
+                        })
+                    }
+                </div>
 
-            <div>
-                <h2 className='text-3xl font-bold'>Sharing of Information</h2>
+                <div>
+                    <h2 className='text-3xl font-bold'>Sharing of Information</h2>
 
-                {
-                    shareInfo?.map((info: any, index: number) => {
-                        return (
-                            <PrivacyContent key={index} data={info} />
+                    {
+                        shareInfo?.map((info: any, index: number) => {
+                            return (
+                                <PrivacyContent key={index} data={info} />
+                            )
+                        }
+
                         )
                     }
 
-                    )
-                }
-
-            </div>
+                </div>
 
 
-            <div>
-                {
-                    contactInfo?.map((info: any, index: number) => {
-                        return (
-                            <div key={index}>
-                                <PrivacyContent data={info} />
-                                <div className="flex flex-col ">
-                                    <span>Email:{" "} <a href="mailto:someone@example.com" className="underline">{info?.email}</a></span>
-                                    <span>Address: {" "} <span>{info?.address}</span></span>
+                <div>
+                    {
+                        contactInfo?.map((info: any, index: number) => {
+                            return (
+                                <div key={index}>
+                                    <PrivacyContent data={info} />
+                                    <div className="flex flex-col ">
+                                        <span>Email:{" "} <a href="mailto:someone@example.com" className="underline">{info?.email}</a></span>
+                                        <span>Address: {" "} <span>{info?.address}</span></span>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
             </div>
-            
+
         </div>
     );
 };
