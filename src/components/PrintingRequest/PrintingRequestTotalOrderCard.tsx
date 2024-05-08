3892cc4cc0c0f63367c0ef673886eb76712ab0a7
Shariful525 <sharifulafsar0@gmail.com>
2024-05-08 18:04:51 +0600

    pulled main branch

--- src/components/PrintingRequest/PrintingRequestTotalOrderCard.tsx
+++ src/components/PrintingRequest/PrintingRequestTotalOrderCard.tsx
@@ -128,11 +128,10 @@ const PrintingRequestTotalOrderCard = ({ buttonText }: any) => {
         className="flex justify-center items-center px-5 py-4   "
       >
         <button
-          className={`bg-gradient-to-r from-[#C83B62] to-[#7F35CD] w-full rounded-lg py-3 text-white  shadow-sm hover:duration-500 hover:shadow-lg text-center ${
-            totalAmountWithQuantity
-              ? "cursor-pointer"
-              : "cursor-not-allowed btn-disabled"
-          }`}
+          className={`bg-gradient-to-r from-[#C83B62] to-[#7F35CD] w-full rounded-lg py-3 text-white  shadow-sm hover:duration-500 hover:shadow-lg text-center ${totalAmountWithQuantity
+            ? "cursor-pointer"
+            : "cursor-not-allowed btn-disabled"
+            }`}
         >
           {buttonText}
         </button>
